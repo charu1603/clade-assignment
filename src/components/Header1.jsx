b6fbb98115af4fa7422000b8bc28369af5cc88a9
John Doe <kakdechaitrali16@gmail.com>
2024-06-25 20:48:44 +0530

    minor changes

--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -5,7 +5,7 @@ const Header1 = () => {
     <header className="bg-white px-16 flex  items-center py-2 border font-medium text-[#888888] text-base w-full">
         <div className='flex items-center '>
             <ul className='flex space-x-12 items-center'>
-                <li className='text-[#DC4A2D] font-bold underline underline-offset-[16px]'>Job preview</li>
+                <li className='text-[#DC4A2D] font-bold underline underline-offset-[13px]'>Job preview</li>
                 <li>Applicants</li>
                 <li>Match</li>
                 <li>Messages</li>
